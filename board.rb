be23bc525b7bfa4447f971d5e29eb53ce39d16e4
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-04-10 15:30:37 -0700

    Update board class to make sure all pieces are on the board at start

--- board.rb
+++ board.rb
@@ -1,33 +1,34 @@
-require_relative "piece"
+require_relative "pieces"
 
 class Board 
+	attr_reader :rows 
 	def initialize
-		@rows = Array.new(8) { Array.new(8) }
-		[0, 1, 6, 7].each do |row|
-			(0..7).each do |column|
-				@rows[row][column] = Piece.new
-			end
-		end
+		@sentinel = NullPiece.instance
+		construct_starting_board
 	end
 
 	def [](position)
 		raise "Invalid position" unless valid_position?(position)
 
-		x, y = position
-		@rows[x][y]
+		row, col = position
+		@rows[row][col]
 	end
 
 	def []=(position, piece)
 		raise "Invalid position" unless valid_position?(position)
 
-		x, y = position 
-		@rows[x][y] = piece
+		row, col = position 
+		@rows[row][col] = piece
 	end
 
-	def move_piece(start_position, end_position)
+	def move_piece(turn_color, start_position, end_position)
+		raise "Start position is empty" if empty?(start_position)
+
 		piece = self[start_position]
-		if piece.nil?
-			raise "No piece found at position #{start_position}, try again."
+		if piece.color != turn_color
+			raise "You must move your own piece"
+		elsif !piece.moves.include?(end_position)
+			raise "Piece does not move like that"
 		end
 
 		self[end_position] = piece
@@ -36,10 +37,45 @@ class Board
 		nil
 	end
 
+	def add_piece(piece, position)
+		raise "Position is not empty" unless empty?(position)
+		self[position] = piece
+	end
+
 	def valid_position?(position)
 		position.all? { |coordinate| coordinate.between?(0, 7) }
 	end
 
+	def empty?(position)
+		self[position].empty?
+	end
+
 	private 
+
+	attr_reader :sentinel
+
+	def fill_back_row(color)
+		back_pieces = [
+			Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
+		]
+
+		i = (color == :white ? 7 : 0)
+		back_pieces.each_with_index do |piece_class, j|
+			piece_class.new(color, self, [i, j])
+		end
+	end
+
+	def fill_front_row(color)
+		i = (color == :white ? 6 : 1)
+		8.times { |j| Pawn.new(color, self, [i, j]) }
+	end
+
+	def construct_starting_board
+		@rows = Array.new(8) { Array.new(8, sentinel) }
+		%i(black white).each do |color|
+			fill_back_row(color)
+			fill_front_row(color)
+		end
+	end
 
 end
