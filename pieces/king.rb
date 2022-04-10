b958184dc9471956e5d56c17d267258eca10ee81
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-04-10 12:02:33 -0700

    Implement king class

---
+++ pieces/king.rb
@@ -0,0 +1,22 @@
+require_relative "stepable"
+require_relative "piece"
+
+class King
+	include Stepable
+
+	def symbol 
+		"♚".colorize(color)
+	end 
+
+	private 
+
+	def move_directions
+		[
+			[-1, -1], [1,  1],
+			[-1,  0], [0, -1],
+			[ 1,  0], [0,  1],
+			[-1,  1], [1, -1]
+		]
+	end
+
+end
